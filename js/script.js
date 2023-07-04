c5ff625ecf19d4307e484c3f898e993c6a8ff83b
SUB <powersub1101@naver.com>
2023-07-04 11:09:55 +0900

    하단 더보기 버튼 업데이트 완료

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// 섹션 1
+// 550 스크롤 도착시 메인1 동영상 페이드
+window.addEventListener('scroll', function() {
+    var videoContainer = document.querySelector('.m-video');
+    var mainVideo = document.getElementById('mainVideo');
+    var scrollY = window.scrollY
+
+    if (scrollY >= 550) {
+      mainVideo.classList.add('dark-video');
+    } else {
+      mainVideo.classList.remove('dark-video');
+    }
+  });
+
+
+// 섹션 2
+// 마우스 호버시 동영상 보이게
+const introduceDivs = document.querySelectorAll("#introduce > div");
+
+introduceDivs.forEach((div) => {
+  const video = div.querySelector("video");
+
+  div.addEventListener("mouseenter", () => {
+    video.classList.add("active");
+  });
+
+  div.addEventListener("mouseleave", () => {
+    video.classList.remove("active");
+  });
+});
+
+// 1500 스크롤 도착시 #introduce opacity 변경
+window.addEventListener('scroll', function() {
+    var introduce = document.getElementById('introduce'); // introduce 요소 선택
+    var scrollPosition = window.scrollY; // 스크롤 위치 확인
+
+    if (scrollPosition >= 1600) {
+      introduce.style.opacity = 1; // opacity 값 변경
+    } else {
+      introduce.style.opacity = 0; // 기본 opacity 값으로 설정
+    }
+  });
